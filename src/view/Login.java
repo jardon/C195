56eac02802df2933471699eb6221c44c6395690e
Jarred Wilson <jarred.wilson12@gmail.com>
2020-02-24 13:06:20 -0500

    Add basic login layout

---
+++ src/view/Login.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML private Label username;
+    @FXML private Label password;
+    @FXML private TextField usernameField;
+    @FXML private TextField passwordField;
+
+    public void login() {
+
+    }
+
+}
