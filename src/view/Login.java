51048f4f4c56fcf138c3f9e0f431a6be694ee19f
Jarred Wilson <jarred.wilson12@gmail.com>
2020-02-25 11:59:54 -0500

    Add connection info to Login page

--- src/view/Login.java
+++ src/view/Login.java
@@ -1,18 +1,28 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ResourceBundle;
 
-public class Login {
+public class Login implements Initializable {
 
     @FXML private Label username;
     @FXML private Label password;
     @FXML private TextField usernameField;
     @FXML private TextField passwordField;
 
-    public void login() {
-
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://3.227.166.251:3306/U07Ebi?profileSQL=true", "U07Ebi", "53689000795");
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 }
