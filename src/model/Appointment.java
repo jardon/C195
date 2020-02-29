7c212e75d6283debaa1289dcad65e33937108a05
Jarred Wilson <jarred.wilson12@gmail.com>
2020-02-28 20:43:17 -0500

    Implement Appointment model

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,4 +1,44 @@
 package model;
 
 public class Appointment {
+
+    private String title;
+    private String description;
+    private String location;
+    private String contact;
+    private String type;
+    private String url;
+
+    public Appointment(String title, String description, String location, String contact, String type, String url) {
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.contact = contact;
+        this.type = type;
+        this.url = url;
+    }
+
+    public String getTitle() { return title; }
+
+    public String getDescription() { return description; }
+
+    public String getLocation() { return location; }
+
+    public String getContact() { return contact; }
+
+    public String getType() { return type; }
+
+    public String getUrl() { return url; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public void setLocation(String location) { this.location = location; }
+
+    public void setContact(String contact) { this.contact = contact; }
+
+    public void setType(String type) { this.type = type; }
+
+    public void setUrl(String url) { this.url = url; }
 }
